e95cac58eb4b79fc0e57a008ebd2d5b24a93e85a
Flavia Fernanda Weber de Souza <flaviafernandaws@gmail.com>
2022-06-28 23:31:50 -0300

    Testes descritos e confirmados para ambas as funções

--- tests/testthat/test-1-mx_weight.R
+++ tests/testthat/test-1-mx_weight.R
@@ -1,3 +1,54 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("error is returned with wrong input class", {
+  mm <- base::matrix(base::sample(1:10,
+                                  size = 100, replace = TRUE), ncol = 10, nrow = 10)
+  map <- terra::rast(mm)
+  m <- map
+  tam <- 100
+  pix <- 30
+  type <- "circle"
+
+  m1 <- c(1:10)
+  expect_error(mx_weight(m = m1, tam, pix, type))
+
+  tam1 <- c(1:10)
+  expect_error(mx_weight(m, tam = tam1, pix, type))
+
+  tam2 <- "circle"
+  expect_error(mx_weight(m, tam = tam2, pix, type))
+
+  pix1 <- c(1:10)
+  expect_error(mx_weight(m, tam, pix = pix1, type))
+
+  pix2 <- "circle"
+  expect_error(mx_weight(m, tam, pix = pix2, type))
+
+  type1 <- c(1:10)
+  expect_error(mx_weight(m, tam, pix, type = type1))
+
+  type2 <- "line"
+  expect_error(mx_weight(m, tam, pix, type = typ2))
 })
+
+
+test_that("returned object class is correct", {
+  mm <- base::matrix(base::sample(1:10,
+                                  size = 100, replace = TRUE), ncol = 10, nrow = 10)
+  map <- terra::rast(mm)
+  m <- map
+  tam <- 100
+  pix <- 30
+  type <- "circle"
+
+  expect_vector(base::mean(terra::res(m))*(tam/pix))
+  expect_vector(terra::focalMat(m, d, type = type))
+  expect_vector(mx_weight(m, tam, pix, type))
+
+  expect_type(base::mean(terra::res(m))*(tam/pix), "double")
+  expect_type(terra::focalMat(m, d, type = type), "double")
+  expect_type(mx_weight(m, tam, pix, type), "double")
+})
+
+
+
+
+
--- tests/testthat/test-2-quality.R
+++ tests/testthat/test-2-quality.R
@@ -1,3 +1,41 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("error is returned with wrong input class", {
+  mm <- base::matrix(base::sample(1:10,
+                                  size = 100, replace = TRUE), ncol = 10, nrow = 10)
+  map <- terra::rast(mm)
+  m <- map
+  fm <- fm
+  lvl <- info$lvl_cod[1]
+  q <- info$q_peso[1]
+  na.rm <- TRUE
+
+  m1 <- c(1:10)
+  expect_error(quality(m = m1, fm, lvl, q, na.rm))
+
+  fm1 <- 2
+  expect_error(quality(m, fm = fm1, lvl, q, na.rm))
+
+  lvl1 <- c(1:10)
+  expect_error(quality(m, fm, lvl = lvl1, q, na.rm))
+
+  q1 <- c(1:10)
+  expect_error(quality(m, fm, lvl, q = q1, na.rm))
+
+  na.rm1 <- 2
+  expect_error(quality(m, fm, lvl, q, na.rm = na.rm1))
+})
+
+
+test_that("returned object class is correct", {
+  mm <- base::matrix(base::sample(1:10,
+                     size = 100, replace = TRUE), ncol = 10, nrow = 10)
+  map <- terra::rast(mm)
+  m <- map
+  fm <- fm
+  lvl <- info$lvl_cod[1]
+  q <- info$q_peso[1]
+  na.rm <- TRUE
+
+  expect_vector(nlevels(m))
+  expect_s4_class(quality(m, fm, lvl, q, na.rm), "SpatRaster")
+  expect_type(quality(m, fm, lvl, q, na.rm), "S4")
 })
